29ce9d2e94b11867375b7ceaaf9ae90863cf98d2
olman2020 <51057022+olman2020@users.noreply.github.com>
2022-10-26 17:50:35 -0600

    Base de datos de los verbos
    Se crea la base de datos de los verbos

---
+++ TransLog/Base de Datos Verbos.pl
@@ -0,0 +1,184 @@
+%ser
+verbo(singular, primera, "soy", "am").
+verbo(singular, segunda, "eres", "are").
+verbo(singular, segunda, "es", "are").
+verbo(singular, tercera, "es", "is").
+verbo(plural, primera, "somos", "are").
+verbo(plural, segunda, "son", "are").
+verbo(plural, tercera, "son", "are").
+
+%estar
+verbo(singular, primera, "estoy", "am").
+verbo(singular, segunda, "estás", "are").
+verbo(singular, segunda, "está", "are").
+verbo(singular, tercera, "está", "is").
+verbo(plural, primera, "estamos", "are").
+verbo(plural, segunda, "están", "are").
+verbo(plural, tercera, "están", "are").
+
+%preguntar
+verbo(singular, primera, "pregunto", "ask").
+verbo(singular, segunda, "preguntas", "ask").
+verbo(singular, segunda, "pregunta", "ask").
+verbo(singular, tercera, "pregunta", "asks").
+verbo(plural, primera, "preguntamos", "ask").
+verbo(plural, segunda, "preguntais", "ask").
+verbo(plural, tercera, "preguntan", "ask").
+
+%llamar
+verbo(singular, primera, "llamo", "call").
+verbo(singular, segunda, "llamas", "call").
+verbo(singular, segunda, "llama", "call").
+verbo(singular, tercera, "llama", "calls").
+verbo(plural, primera, "llamamos", "call").
+verbo(plural, segunda, "llaman", "call").
+verbo(plural, tercera, "llaman", "call").
+
+%venir
+verbo(singular, primera, "vengo", "come").
+verbo(singular, segunda, "vienes", "come").
+verbo(singular, segunda, "viene", "come").
+verbo(singular, tercera, "viene", "comes").
+verbo(plural, primera, "venimos", "come").
+verbo(plural, segunda, "vienen", "come").
+verbo(plural, tercera, "vienen", "come").
+
+%hacer/* verificar como agregar do sin afectar make*/
+verbo(singular, primera, "hago", "make").
+verbo(singular, segunda, "haces", "make").
+verbo(singular, segunda, "hace", "make").
+verbo(singular, tercera, "hace", "makes").
+verbo(plural, primera, "hacemos", "make").
+verbo(plural, segunda, "hacen", "make").
+verbo(plural, tercera, "hacen", "make").
+
+%sentir
+verbo(singular, primera, "siento", "feels").
+verbo(singular, segunda, "sientes", "feels").
+verbo(singular, segunda, "siente", "feels").
+verbo(singular, tercera, "siente", "feels").
+verbo(plural, primera, "sentimos", "feels").
+verbo(plural, segunda, "sienten", "feels").
+verbo(plural, tercera, "sienten", "feels").
+
+%encontrar
+verbo(singular, primera, "encuentro", "find").
+verbo(singular, segunda, "encuentras", "find").
+verbo(singular, segunda, "encuentra", "find").
+verbo(singular, tercera, "encuentra", "finds").
+verbo(plural, primera, "encontramos", "find").
+verbo(plural, segunda, "encuentran", "find").
+verbo(plural, tercera, "encuentran", "find").
+
+%obtener
+verbo(singular, primera, "obtengo", "get").
+verbo(singular, segunda, "obtienes", "get").
+verbo(singular, segunda, "obtiene", "get").
+verbo(singular, tercera, "obtiene", "gets").
+verbo(plural, primera, "obtenemos", "get").
+verbo(plural, segunda, "obtienen", "get").
+verbo(plural, tercera, "obtienen", "get").
+
+%dar
+verbo(singular, primera, "doy", "give").
+verbo(singular, segunda, "das", "give").
+verbo(singular, segunda, "da", "give").
+verbo(singular, tercera, "da", "gives").
+verbo(plural, primera, "damos", "give").
+verbo(plural, segunda, "dan", "give").
+verbo(plural, tercera, "dan", "give").
+
+%ir
+verbo(singular, primera, "voy", "go").
+verbo(singular, segunda, "vas", "go").
+verbo(singular, segunda, "va", "go").
+verbo(singular, tercera, "va", "goes").
+verbo(plural, primera, "vamos", "go").
+verbo(plural, segunda, "van", "go").
+verbo(plural, tercera, "van", "go").
+
+%tener
+verbo(singular, primera, "tengo", "have").
+verbo(singular, segunda, "tienes", "have").
+verbo(singular, segunda, "tiene", "have").
+verbo(singular, tercera, "tiene", "has").
+verbo(plural, primera, "tenemos", "have").
+verbo(plural, segunda, "tienen", "have").
+verbo(plural, tercera, "tienen", "have").
+
+%saber
+verbo(singular, primera, "sé", "know").
+verbo(singular, segunda, "sabes", "know").
+verbo(singular, segunda, "sabe", "know").
+verbo(singular, tercera, "sabe", "knows").
+verbo(plural, primera, "sabemos", "knows").
+verbo(plural, segunda, "saben", "know").
+verbo(plural, tercera, "saben", "know").
+
+%conocer
+verbo(singular, primera, "conozco", "know").
+verbo(singular, segunda, "conoces", "know").
+verbo(singular, segunda, "conoce", "know").
+verbo(singular, tercera, "conoce", "knows").
+verbo(plural, primera, "conocemos", "know").
+verbo(plural, segunda, "conocen", "know").
+verbo(plural, tercera, "conocen", "know").
+
+%dejar
+verbo(singular, primera, "dejo", "leave").
+verbo(singular, segunda, "dejas", "leave").
+verbo(singular, segunda, "deja", "leave").
+verbo(singular, tercera, "deja", "leaves").
+verbo(plural, primera, "dejamos", "leave").
+verbo(plural, segunda, "dejan", "leave").
+verbo(plural, tercera, "dejan", "leave").
+
+%mirar
+verbo(singular, primera, "miro", "look").
+verbo(singular, segunda, "miras", "look").
+verbo(singular, segunda, "mira", "look").
+verbo(singular, tercera, "mira", "looks").
+verbo(plural, primera, "miramos", "look").
+verbo(plural, segunda, "miran", "look").
+verbo(plural, tercera, "miran", "look").
+
+%decir /* hablar con los demas acerca de tell y say*/
+verbo(singular, primera, "digo", "say").
+verbo(singular, segunda, "dices", "say").
+verbo(singular, segunda, "dice", "say").
+verbo(singular, tercera, "dice", "says").
+verbo(plural, primera, "decimos", "say").
+verbo(plural, segunda, "dicen", "say").
+verbo(plural, tercera, "dicen", "say").
+
+%ver
+verbo(singular, primera, "veo", "see").
+verbo(singular, segunda, "ves", "see").
+verbo(singular, segunda, "ve", "see").
+verbo(singular, tercera, "ve", "sees").
+verbo(plural, primera, "vemos", "see").
+verbo(plural, segunda, "ven", "see").
+verbo(plural, tercera, "ven", "see").
+
+%parecer
+verbo(singular, primera, "parezco", "look like").
+verbo(singular, segunda, "pareces", "look like").
+verbo(singular, segunda, "parece", "look like").
+verbo(singular, tercera, "parece", "looks like").
+verbo(plural, primera, "parecemos", "look like").
+verbo(plural, segunda, "parecen", "look like").
+verbo(plural, tercera, "parecen", "look like").
+
+%tomar
+verbo(singular, primera, "tomo", "take").
+verbo(singular, segunda, "tomas", "take").
+verbo(singular, segunda, "toma", "take").
+verbo(singular, tercera, "toma", "takes").
+verbo(plural, primera, "tomamos", "take").
+verbo(plural, segunda, "toman", "take").
+verbo(plural, tercera, "toman", "take").
+
+
+
+
+
